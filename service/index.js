24be0b3ec82b4eb2dbad16d09cc195dc50afbb61
qinkai <937817514@qq.com>
2022-12-13 08:48:37 +0800

    update: ignore

--- service/index.js
+++ service/index.js
@@ -1,14 +1,14 @@
 export default class Instance {
 	constructor() {}
-	
+
 	static new() {
 		return new this()
 	}
-	
-	
+
+
 	async createRequest(actionFunName, props, catchFunc, endActionFunc) {
 		try {
-			const res = await actionFunName(props)			
+			const res = await actionFunName(props)
 			catchFunc(res, null)
 		} catch (error) {
 			catchFunc(error)
@@ -16,4 +16,6 @@ export default class Instance {
 			endActionFunc && endActionFunc()
 		}
 	}
+
+
 }
